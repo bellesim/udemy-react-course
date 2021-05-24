355c34dfb12d00314236e2ae694d2f6649d93c5f
Belle Sim <bellesimbeier@gmail.com>
2021-05-24 18:28:36 +0800

    Assignment 1

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,18 @@
-function ExpenseItem() {
-    return <h2>Expense Item!</h2>
-}
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 
+//pass one parameter known as props
+//props holds all attributes of the element
+//props can access each attribute name
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+          <ExpenseDate date={props.date}/>
+      <div className="expense-item__description">
+        <h2 className="expense-item">{props.title}</h2>
+        <div className="expense-item__price">${props.amount} </div>
+      </div>
+    </div>
+  );
+}
 export default ExpenseItem;
